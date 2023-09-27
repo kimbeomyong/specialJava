3c0de81f5b923088b7e22c07815e18e63ed34799
kimbeomyong <qkfmf@naver.com>
2023-09-27 11:33:25 +0900

    [create ArrayList Test exam5]
    7장 연습문제 5번 프로그램

---
+++ javaTest/src/ch07/exam05_1/Student.java
@@ -0,0 +1,80 @@
+package ch07.exam05_1;
+
+import java.util.Objects;
+
+public class Student {
+	private String name;
+	private String major;
+	private int schoolNum;
+	private double gradeAvg;
+
+	public Student() {
+		this(null, null, 0, 0.0);
+	}
+
+	public Student(String name, int id) {
+		this(name, null, id, 0.0);
+
+	}
+
+	public Student(String name, String major, int schoolNum, double gradeAvg) {
+		super();
+		this.name = name;
+		this.major = major;
+		this.schoolNum = schoolNum;
+		this.gradeAvg = gradeAvg;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getMajor() {
+		return major;
+	}
+
+	public void setMajor(String major) {
+		this.major = major;
+	}
+
+	public int getSchoolNum() {
+		return schoolNum;
+	}
+
+	public void setSchoolNum(int schoolNum) {
+		this.schoolNum = schoolNum;
+	}
+
+	public double getGradeAvg() {
+		return gradeAvg;
+	}
+
+	public void setGradeAvg(double gradeAvg) {
+		this.gradeAvg = gradeAvg;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(name, schoolNum);
+	}
+
+	@Override
+		public boolean equals(Object obj) {
+			Student student = null;
+			if(!(obj instanceof Student)) {
+				return false;
+			}
+			student = (Student)obj;
+			return (this.name.equals(student.name) && this.schoolNum == student.schoolNum);
+	}
+	@Override
+	public String toString() {
+		return name + ", " + major + ", " + schoolNum + ", " + gradeAvg;
+
+	}
+
+}
